01c26abfff2abccd4dedbabf88200b2d207da46a
Serge <osr@list.ru>
2018-01-10 22:06:18 +0700

    Подключены гусеницы и локатор!

--- Servo/servo_test/servo_test.ino
+++ Servo/servo_test/servo_test.ino
@@ -3,19 +3,19 @@
 // двигатели
 
 const char pinLeftEng    = 5;
-const char pinLeftEngFrd = 6;
-const char pinLeftEngBck = 7;
+const char pinLeftEngFrd = 7;
+const char pinLeftEngBck = 6;
 
-const char pinRightEng    = 9;
-const char pinRightEngFrd = 10;
-const char pinRightEngBck = 8;
+const char pinRightEng    = 11;
+const char pinRightEngFrd = 8;
+const char pinRightEngBck = 9;
 
 const char ENG_MIN_SPEED = 150;
 const char ENG_MAX_SPEED = 250;
 
 
-const char PinServo = 2;
-const char pinEcho  = 3;
+const char PinServo = 3;
+const char pinEcho  = 2;
 const char pinTring = 4;
 
 // управление двигателем 
@@ -66,28 +66,90 @@ int Radar(void)
   return mm;
 }
 
+
+void ManagerEngine(Engine en, EngineCommand cmd, unsigned char Speed)
+{
+  char pinEn  = 0;
+  char pinFrd = 0;
+  char pinBck = 0;
+  
+  switch(en) {
+    case enLeft:
+      pinEn  = pinLeftEng;
+      pinFrd = pinLeftEngFrd;
+      pinBck = pinLeftEngBck;
+      break;
+    case enRight:
+      pinEn  = pinRightEng;
+      pinFrd = pinRightEngFrd;
+      pinBck = pinRightEngBck;
+      break;
+  };
+
+  switch(cmd) {
+    case ENG_FORWARD:
+      digitalWrite(pinFrd, HIGH); 
+      digitalWrite(pinBck, LOW); 
+      analogWrite(pinEn, Speed);
+      break;
+    case ENG_BACKWARD:
+      digitalWrite(pinFrd, LOW); 
+      digitalWrite(pinBck, HIGH); 
+      analogWrite(pinEn, Speed);
+      break;
+    case ENG_STOP:
+      digitalWrite(pinFrd, LOW); 
+      digitalWrite(pinBck, LOW); 
+      analogWrite(pinEn,     0);
+      break;
+  };
+}
+
+
 void setup() {
  ServoMotor.attach(PinServo);
+ pinMode(LED_BUILTIN, OUTPUT);
+
  CurAngle = 90;
  ServoMotor.write(CurAngle);
  delay(1000);  
- pinMode(LED_BUILTIN, OUTPUT);
 
+ 
  // инициализация портов для эхо локации
  pinMode(pinTring, OUTPUT); 
  pinMode(pinEcho,  INPUT); 
 
  digitalWrite(pinTring, LOW); 
  digitalWrite(pinEcho,  LOW); 
+
+  // инициализация портов для двигателей
+ pinMode(pinLeftEng,     OUTPUT);  
+ pinMode(pinLeftEngFrd,  OUTPUT);  
+ pinMode(pinLeftEngBck,  OUTPUT);  
+ pinMode(pinRightEng,    OUTPUT);  
+ pinMode(pinRightEngFrd, OUTPUT);  
+ pinMode(pinRightEngBck, OUTPUT);  
+
+ digitalWrite(pinLeftEng,     LOW);
+ digitalWrite(pinLeftEngFrd,  LOW);  
+ digitalWrite(pinLeftEngBck,  LOW);  
+ digitalWrite(pinRightEng,    LOW);  
+ digitalWrite(pinRightEngFrd, LOW);  
+ digitalWrite(pinRightEngBck, LOW);  
 }
 
 void loop() 
 {
   digitalWrite(LED_BUILTIN, LOW); 
- /* if (Radar() <= 250) 
+
+
+ ManagerEngine(enLeft, ENG_FORWARD, 128);
+ 
+  
+  if (Radar() <= 250) 
   {
      digitalWrite(LED_BUILTIN, HIGH);    
      delay(2000);  
-  } */
+  } 
 
 }
